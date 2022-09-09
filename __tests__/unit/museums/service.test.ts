3ca5411090e587a364144bf4389808b3310505b8
Joseph Gay <joseph@josephgay.net>
2022-09-09 07:20:20 -0400

    create domain object service class w/ tdd
    create src/domainObj/service.ts
    
    import domain object service into unit test and watch it fail.
    
    create domain object service class and watch pass.

--- __tests__/unit/museums/service.test.ts
+++ __tests__/unit/museums/service.test.ts
@@ -1,4 +1,5 @@
 import { describe, it } from "../../../dev_deps.ts";
+import { Service as MuseumService } from "../../../src/museums/service.ts";
 
 describe("MuseumService.findAll", () => {
   it("should return a list of museums", async () => {
