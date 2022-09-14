837d156177abc5a58ef608238dac885ceda4210f
Joseph Gay <joseph@josephgay.net>
2022-09-14 19:31:52 -0400

    externalize config

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,23 @@
 import { Server } from "./framework/mod.ts";
+import { config } from "std/dotenv/mod.ts";
 import { healthcheck, museums } from "./routes/mod.ts";
+
+const env = await config({ safe: true })
 
 const server = new Server({
   configuration: {
     app: {
-      name: "Museums App",
-      port: 8080,
-      apiPrefix: "/api/v1",
+      name: env.APP_NAME,
+      port: parseInt(env.APP_PORT),
+      apiPrefix: env.API_PREFIX,
     },
     https: {
-      secure: true,
-      certFile: "./certificate.pem",
-      keyFile: "./private.pem",
+      secure: env.HTTPS_ENABLED === "true",
+      certFile: env.TLS_CERT,
+      keyFile: env.TLS_KEY,
     },
     allowedOrigins: [
-      "http://localhost:8080",
+      env.LOCALHOST,
     ],
     endpoints: [
       healthcheck,
